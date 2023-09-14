87de50a678ff7c6cdf3d8851590c849a256ae649
nsw99 <133620206+nsw99@users.noreply.github.com>
2023-09-14 12:03:11 +0900

    09-14

---
+++ 7-jsx/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+const App = () =>{
+  const [counter, setCounter] = useState(0);
+  const setCount = () => {
+    setCounter(counter + 1);  
+};
+  return (
+    <div>
+        <h1>Total clicks: {counter}</h1>
+        <button onClick={setCount}>Click!</button>
+    </div>
+  );
+};
+export default App;        
